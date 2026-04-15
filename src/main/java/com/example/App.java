e2d707910f67d662892d51d66015ea1d1afc42be
SUHAS N M <133192948+SuhasNazare@users.noreply.github.com>
2026-04-15 22:57:36 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,40 +3,42 @@ package com.example;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class App {
     public static void main(String[] args) throws InterruptedException {
+        
+        ChromeOptions options = new ChromeOptions();
+        
+        options.addArguments("--headless=new"); 
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--window-size=1920,1080");
 
-	WebDriver driver = new ChromeDriver();
-        // Open Products Page
+        WebDriver driver = new ChromeDriver(options);
+
         driver.get("https://automationexercise.com/products");
-        // Maximize Browser Window
+        
         driver.manage().window().maximize();
-        // Add Product 4 to Cart
+        
         driver.findElement(By.cssSelector("[data-product-id='4']")).click();
         Thread.sleep(2000);
         driver.findElement(By.cssSelector(".btn.btn-success.close-modal.btn-block")).click();
 
-        // Add Product 5 to Cart
         driver.findElement(By.cssSelector("[data-product-id='5']")).click();
         Thread.sleep(2000);
         driver.findElement(By.cssSelector(".btn.btn-success.close-modal.btn-block")).click();
 
-        // Add Product 6 to Cart
         driver.findElement(By.cssSelector("[data-product-id='6']")).click();
         Thread.sleep(2000);
         driver.findElement(By.cssSelector(".btn.btn-success.close-modal.btn-block")).click();
 
-        // Open Cart Page
         driver.get("https://automationexercise.com/view_cart");
 
-        // Refresh Cart Page
         driver.navigate().refresh();
 
-        // Wait for 10 seconds
         Thread.sleep(10000);
 
-        // Close Browser
         driver.quit();
     }
 }
